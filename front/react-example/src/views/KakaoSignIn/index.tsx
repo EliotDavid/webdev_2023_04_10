edfc0b1e26887b1e513630826816f8c96b6657c1
devjiraynor <jiraynor@naver.com>
2023-02-02 16:50:17 +0900

    2023.02.02. 16:50

---
+++ front/react-example/src/views/KakaoSignIn/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default function KakaoSignIn() {
+  return (
+    <div className='kakao-container'>
+        {/* 로그인 폼 레이아웃 */}
+        <div className='kakao-login-form'>
+            {/* 로고 레이아웃 */}
+            <div className='kakao-logo'>
+
+            </div>
+            {/* 로그인 레이아웃 */}
+            <div className='kakao-login'>
+                {/* 인풋 레이아웃 */}
+                <div className='kakao-input-layout'>
+
+                </div>
+                {/* 버튼 레이아웃 */}
+                <div className='kakao-button-layout'>
+
+                </div>
+                {/* 링크 레이아웃 */}
+                <div className='kakao-link-layout'>
+
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
