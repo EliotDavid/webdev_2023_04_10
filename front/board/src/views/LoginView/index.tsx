8550c87fbb13cee7e923f98ddfed4e613985ddc6
devjiraynor <jiraynor@naver.com>
2023-02-07 15:05:13 +0900

    2023.02.07. 14:50

--- front/board/src/views/LoginView/index.tsx
+++ front/board/src/views/LoginView/index.tsx
@@ -2,10 +2,9 @@ import { useState } from 'react'
 //? Box 컴포넌트 : mui에서 공간을 할당하는 모든 태그를 포함
 //? Grid 컴포넌트 : mui에서 공간을 12등분하여 가로 사이즈에 따라 반응형 웹 개발을 지원
 //? Typography 컴포넌트 : mui에서 글자를 출력하는 모든 태그를 포함
-import { Box, Card, Grid, TextField, Typography, FormControl, InputLabel, Input, InputAdornment, IconButton } from '@mui/material';
+import { Box, Card, Grid, TextField, Typography, FormControl, InputLabel, Input, InputAdornment, IconButton, Button } from '@mui/material';
 import ContentPasteTwoToneIcon from '@mui/icons-material/ContentPasteTwoTone';
-import VisibilityOff from '@mui/icons-material/VisibilityOff';
-import Visibility from '@mui/icons-material/Visibility';
+
 
 export default function LoginView() {
     const [showPassword, setShowPassword] = useState<boolean>(false);
@@ -20,27 +19,35 @@ export default function LoginView() {
                 </Box>
             </Grid>
             <Grid item lg={5} sm={12}>
-                <Card sx={{height: '710px', mt: '100px', mb: '80px', pt: '50px', pb: '30px', pl: '50px', pr: '50px'}}>
-                    <Typography variant='h5' fontWeight='900'>로그인</Typography>
-                    <TextField sx={{mt: '40px'}} fullWidth label="이메일 주소" variant="standard" />
-                    
-                    <FormControl sx={{ m: 1, width: '25ch' }} variant="standard">
-                        <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
-                        <Input
-                            type={showPassword ? 'text' : 'password'}
-                            endAdornment={
-                            <InputAdornment position="end">
-                                <IconButton
-                                aria-label="toggle password visibility"
-                                onClick={() => setShowPassword(!showPassword)}
-                                >
-                                {showPassword ? <VisibilityOff /> : <Visibility />}
-                                </IconButton>
-                            </InputAdornment>
-                            }
-                        />
-                    </FormControl>
-
+                <Card sx={{height: '630px', mt: '100px', mb: '80px', pt: '50px', pb: '30px', pl: '50px', pr: '50px'}}>
+                    {/*//? 카드안의 요소를 분리할 부모 공간 */}
+                    <Box display='flex' sx={{height: '100%', flexDirection: 'column', justifyContent: 'space-between'}}>
+                        {/*//? 인풋 공간 */}
+                        <Box>
+                            <Typography variant='h5' fontWeight='900'>로그인</Typography>
+                            <TextField sx={{mt: '40px'}} fullWidth label="이메일 주소" variant="standard" />
+                            <FormControl fullWidth variant="standard" sx={{ mt: '40px' }}>
+                                <InputLabel>비밀번호</InputLabel>
+                                <Input
+                                    type={showPassword ? 'text' : 'password'}
+                                    endAdornment={
+                                    <InputAdornment position="end">
+                                        <IconButton
+                                        onClick={() => setShowPassword(!showPassword)}
+                                        >
+                                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                                        </IconButton>
+                                    </InputAdornment>
+                                    }
+                                />
+                            </FormControl>
+                        </Box>
+                         {/*//? 버튼 공간 */}
+                        <Box>
+                            <Button sx={{mb: '20px'}} fullWidth variant="contained" size='large'>로그인</Button>
+                            <Typography textAlign={'center'}>신규사용자 이신가요? <Typography component='span' fontWeight={900}>회원가입</Typography></Typography>
+                        </Box>
+                    </Box>
                 </Card>
             </Grid>
         </Grid>
