ab094c0e64c084e4fa50247615e6ca7f29d43e8c
devjiraynor <jiraynor@naver.com>
2023-02-08 17:10:32 +0900

    2023.02.08. 17:10

--- front/board/src/views/AuthenticationView/SignUpCardView/index.tsx
+++ front/board/src/views/AuthenticationView/SignUpCardView/index.tsx
@@ -12,11 +12,13 @@ import {
 } from "@mui/material";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
 import Visibility from "@mui/icons-material/Visibility";
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 
 import { useSignUpStore } from 'src/stores';
 
 function FirstPage() {
 
+  const { email, password, passwordCheck } = useSignUpStore();
   const { setEmail, setPassword, setPasswordCheck } = useSignUpStore();
 
   const [showPassword, setShowPassword] = useState<boolean>(false);
@@ -29,6 +31,7 @@ function FirstPage() {
         fullWidth
         label="이메일 주소*"
         variant="standard"
+        value={email}
         onChange={(event) => setEmail(event.target.value)}
       />
       <FormControl fullWidth variant="standard" sx={{ mt: "40px" }}>
@@ -42,6 +45,7 @@ function FirstPage() {
               </IconButton>
             </InputAdornment>
           }
+          value={password}
           onChange={(event) => setPassword(event.target.value)}
         />
       </FormControl>
@@ -58,6 +62,7 @@ function FirstPage() {
               </IconButton>
             </InputAdornment>
           }
+          value={passwordCheck}
           onChange={(event) => setPasswordCheck(event.target.value)}
         />
       </FormControl>
@@ -66,7 +71,32 @@ function FirstPage() {
 }
 
 function SecondPage() {
-  return <Box>두번째 페이지</Box>;
+
+  const { nickName, telNumber, address, addressDetail } = useSignUpStore();
+  const { setNickName, setTelNumber, setAddress, setAddressDetail } = useSignUpStore();
+
+  return (
+    <Box>
+      <TextField sx={{mt: '40px'}} fullWidth label="닉네임*" variant="standard" value={nickName} onChange={(event) => setNickName(event.target.value)} />
+      <TextField sx={{mt: '40px'}} fullWidth label="휴대폰 번호*" variant="standard" value={telNumber} onChange={(event) => setTelNumber(event.target.value)} />
+      
+      <FormControl fullWidth variant="standard" sx={{mt: '40px'}}>
+        <InputLabel>주소*</InputLabel>
+        <Input type="text" endAdornment={
+          <InputAdornment position="end">
+            <IconButton>
+              <KeyboardArrowRightIcon />
+            </IconButton>
+          </InputAdornment>
+        } 
+        value={address}
+        onChange={(event) => setAddress(event.target.value)}
+        />
+      </FormControl>
+      
+      <TextField sx={{mt: '40px'}} fullWidth label="상세 주소*" variant="standard" value={addressDetail} onChange={(event) => setAddressDetail(event.target.value)} />
+    </Box>
+  );
 }
 
 interface Props {
@@ -76,12 +106,20 @@ interface Props {
 export default function SignUpCardView({ setLoginView }: Props) {
   const [page, setPage] = useState<number>(1);
 
-  const {  } = useSignUpStore();
+  const { email, password, passwordCheck } = useSignUpStore();
 
   const onNextButtonHandler = () => {
-    //todo: 이메일 / 비밀번호 / 비밀번호 확인 검증
-    //todo: 검증이 실패하면 return
-    //todo: 검증이 성공하면 page 변경
+    //? 해당 문자열 변수가 빈값인지 확인
+    //? 1. 해당 변수 == '';
+    //? 2. 해당 변수의 길이 == 0;
+    if (!email || !password || !passwordCheck) {
+      alert('모든 값을 입력하세요.');
+      return;
+    }
+    if (password !== passwordCheck) {
+      alert('비밀번호가 서로 다릅니다.');
+      return;
+    }
     setPage(2);
   };
 
