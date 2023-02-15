844432051f3aed730fd1d93ddf98f93e9958bfff
devjiraynor <jiraynor@naver.com>
2023-02-15 17:49:37 +0900

    2023.02.15. 17:50

--- front/board/src/views/Main/MainHead/index.tsx
+++ front/board/src/views/Main/MainHead/index.tsx
@@ -1,10 +1,20 @@
 import { Box, Card, Grid, Typography } from '@mui/material'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PreviewCard from 'src/components/PreviewCard'
+import { IPreviewItem } from 'src/interfaces';
+
+import { TOP3 } from 'src/mock';
 
 export default function MainHead() {
+
+    const [top3List, setTop3List] = useState<IPreviewItem[]>([]);
+
+    useEffect(() => {
+        setTop3List(TOP3);
+    }, []);
+
   return (
-    <Box sx={{ pb: '40px' }}>
+    <Box sx={{ pb: '40px', pl: '120px', pr: '120px' }}>
         {/* 점보트론 텍스트 */}
         <Box sx={{ pt: '80px', pb: '32px', textAlign: 'center' }}>
             <Typography sx={{ fontSize: '40px', fontWeight: 400 }}>Hoons Board에서</Typography>
@@ -14,15 +24,11 @@ export default function MainHead() {
         <Box>
             <Typography sx={{ fontSize: '24px', fontWeight: 400, p: '24px', textAlign: 'center' }}>주간 TOP 3 게시물</Typography>
             <Grid container spacing={3}>
-                <Grid item sm={12} md={4}>
-                    <PreviewCard />
-                </Grid>
-                <Grid item sm={12} md={4}>
-                    <PreviewCard />
-                </Grid>
-                <Grid item sm={12} md={4}>
-                    <PreviewCard />
-                </Grid>
+                {top3List.map((item) => (
+                    <Grid item sm={12} md={4}>
+                        <PreviewCard previewItem={item} />
+                    </Grid>
+                ))}
             </Grid>
         </Box>
     </Box>
