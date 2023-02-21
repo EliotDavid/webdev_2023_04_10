1521349a0f7028dcefe11a2a689ae05cdc30eb85
devjiraynor <jiraynor@naver.com>
2023-02-21 15:50:05 +0900

    2023.02.21. 15:50

--- front/board/src/views/Board/BoardWriteView/index.tsx
+++ front/board/src/views/Board/BoardWriteView/index.tsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import { useState } from 'react'
+
+import { Box, Divider, Fab, IconButton, Input } from '@mui/material';
+import ImageOutlinedIcon from '@mui/icons-material/ImageOutlined';
+import CreateIcon from '@mui/icons-material/Create';
 
 export default function BoardWriteView() {
+
+  const [boardTitle, setBoardTitle] = useState<string>('');
+  const [boardContent, setBoardContent] = useState<string>('');
+
+  const onWriteHandler = () => {
+    //? 제목 및 내용 검증 (값이 존재하는지)
+
+    //? 존재한다면 작성
+  }
+
   return (
-    <div>BoardWriteView</div>
+    <Box sx={{ p: '0px 198px', backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
+      <Box sx={{ p: '100px 24px', backgroundColor: '#ffffff' }}>
+        <Input fullWidth disableUnderline placeholder='제목을 입력하세요.' sx={{ fontSize: '32px', fontWeight: 500 }} onChange={(event) => setBoardTitle(event.target.value)} />
+        <Divider sx={{ m: '40px 0px' }} />
+        <Box sx={{ display: 'flex', alignItems: 'start' }}>
+          <Input fullWidth disableUnderline multiline minRows={20} placeholder='본문을 작성해주세요.' sx={{ fontSize: '18px', fontWeight: 500, lineHeight: '150%' }} onChange={(event) => setBoardContent(event.target.value)}/>
+          <IconButton>
+            <ImageOutlinedIcon />
+          </IconButton>
+        </Box>
+      </Box>
+      <Fab sx={{ position: 'fixed', bottom: '200px', right: '248px', backgroundColor: 'rgba(0, 0, 0, 0.4)' }}>
+        <CreateIcon />
+      </Fab>
+    </Box>
   )
 }
