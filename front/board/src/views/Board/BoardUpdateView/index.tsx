8f90842d2e9a379b9a2c23adc36a705edbef1193
devjiraynor <jiraynor@naver.com>
2023-02-21 16:50:53 +0900

    2023.02.21. 16:50

--- front/board/src/views/Board/BoardUpdateView/index.tsx
+++ front/board/src/views/Board/BoardUpdateView/index.tsx
@@ -1,27 +1,53 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
 
 import { Box, Divider, Fab, IconButton, Input } from '@mui/material';
 import ImageOutlinedIcon from '@mui/icons-material/ImageOutlined';
 import CreateIcon from '@mui/icons-material/Create';
-import { useNavigate } from 'react-router-dom';
 
-export default function BoardWriteView() {
+import { BOARD_LIST } from 'src/mock';
+import { useUserStore } from 'src/stores';
+
+export default function BoardUpdateView() {
 
   const [boardTitle, setBoardTitle] = useState<string>('');
   const [boardContent, setBoardContent] = useState<string>('');
+  const { user } = useUserStore();
 
+  const { boardNumber } = useParams();
   const navigator = useNavigate();
 
-  const onWriteHandler = () => {
-    //? 제목 및 내용 검증 (값이 존재하는지)
-    if (!boardTitle.trim() || !boardContent.trim()) {
-      alert('모든 내용을 입력해주세요.');
+  useEffect(() => {
+    //? 정상적이지 않은 경로로 접근을 시도했을 때
+    //? main 화면으로 돌려보냄
+    if (!boardNumber) {
+      navigator('/');
       return;
     }
-    //? 존재한다면 작성
-    navigator('/myPage');
-  }
+    //? pathVariable로 전달받은 boardNumber에 해당하는 board 데이터를 검색해 옴
+    const board = BOARD_LIST.find((item) => item.boardNumber === parseInt(boardNumber));
+    //? 검색결과가 존재하지 않으면
+    //? main 화면으로 돌려보냄
+    if (!board) {
+      navigator('/');
+      return;
+    }
+    //? 현재 로그인되어 있는지 검증
+    if (!user) {
+      navigator('/auth');
+      return;
+    }
+    //? 검색된 board의 작성자가 로그인한 user와 일치하는지 검증
+    if (board.writerNickname !== user.nickname) {
+      navigator('/');
+      return;
+    }
 
+    setBoardTitle(board.boardTitle);
+    setBoardContent(board.boardContent);
+  }, []);
+
+  //? 일반적으로 수정페이지는 작성페이지와 거의 똑같음
   return (
     <Box sx={{ p: '0px 198px', backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
       <Box sx={{ p: '100px 24px', backgroundColor: '#ffffff' }}>
