e5f10a179cb8288c90cb277b88a85c2d74dc5a32
devjiraynor <jiraynor@naver.com>
2023-02-22 17:05:03 +0900

    2023.02.22. 16:50

--- front/board/src/views/Board/BoardDetailView/index.tsx
+++ front/board/src/views/Board/BoardDetailView/index.tsx
@@ -1,23 +1,32 @@
 import { MouseEvent, useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom';
 
-import { Avatar, Box, Divider, IconButton, Menu, MenuItem, Typography } from '@mui/material'
+import { Avatar, Box, Card, Divider, IconButton, Menu, MenuItem, Typography } from '@mui/material'
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import CommentOutlinedIcon from '@mui/icons-material/CommentOutlined';
 import KeyboardArrowDownOutlinedIcon from '@mui/icons-material/KeyboardArrowDownOutlined';
 import KeyboardArrowUpOutlinedIcon from '@mui/icons-material/KeyboardArrowUpOutlined';
-import { useNavigate, useParams } from 'react-router-dom';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+
 import { BOARD_LIST } from 'src/mock';
 import { IPreviewItem } from 'src/interfaces';
+import { useUserStore } from 'src/stores';
 
 export default function BoardDetailView() {
 
     const [anchorElement, setAnchorElement] = useState<null | HTMLElement>(null);
+    const [menuFlag, setMenuFlag] = useState<boolean>(false);
     const [menuOpen, setMenuOpen] = useState<boolean>(false);
     const [board, setBoard] = useState<null | IPreviewItem>(null);
+    const [likeStatus, setLikeStatus] = useState<boolean>(false);
+    const [openLike, setOpenLike] = useState<boolean>(false);
+    const [openComment, setOpenComment] = useState<boolean>(false);
 
     const { boardNumber } = useParams();
     const navigator = useNavigate();
+
+    const { user } = useUserStore();
 
     const onMenuClickHandler = (event: MouseEvent<HTMLButtonElement>) => {
         setAnchorElement(event.currentTarget);
@@ -42,6 +51,9 @@ export default function BoardDetailView() {
             navigator('/');
             return;
         }
+
+        const owner = user !== null && user.nickname === board.writerNickname;
+        setMenuFlag(owner);
         setBoard(board);
     }, [])
 
@@ -57,11 +69,13 @@ export default function BoardDetailView() {
                         <Divider sx={{ mr: '8px' }} orientation='vertical' variant='middle' flexItem />
                         <Typography sx={{ fontSize: '16px', fontWeight: 400, opacity: 0.4 }}>{board?.writeDate}</Typography>
                     </Box>
-                    <IconButton onClick={(event) => onMenuClickHandler(event)}>
-                        <MoreVertIcon />
-                    </IconButton>
+                    { menuFlag && (
+                        <IconButton onClick={(event) => onMenuClickHandler(event)}>
+                            <MoreVertIcon />
+                        </IconButton>
+                    ) }
                     <Menu anchorEl={anchorElement} open={menuOpen} onClose={onMenuCloseHandler}>
-                        <MenuItem sx={{ p: '10px 59px', opacity: 0.5 }}>수정</MenuItem>
+                        <MenuItem sx={{ p: '10px 59px', opacity: 0.5 }} onClick={() => navigator(`/board/update/${board?.boardNumber}`)}>수정</MenuItem>
                         <Divider />
                         <MenuItem sx={{ p: '10px 59px', color: '#ff0000', opacity: 0.5 }}>삭제</MenuItem>
                     </Menu>
@@ -74,23 +88,42 @@ export default function BoardDetailView() {
             </Box>
             <Box sx={{ display: 'flex', mt: '20px' }}>
                 <Box sx={{ mr: '20px', display: 'flex' }}>
-                    <FavoriteBorderIcon sx={{ height: '24px', width: '24px', mr: '6px', opacity: 0.7 }} />
+                    { likeStatus ? 
+                        (<FavoriteIcon sx={{ height: '24px', width: '24px', mr: '6px', opacity: 0.7, color: '#ff0000' }} onClick={() => setLikeStatus(!likeStatus)} />) : 
+                        (<FavoriteBorderIcon sx={{ height: '24px', width: '24px', mr: '6px', opacity: 0.7 }} onClick={() => setLikeStatus(!likeStatus)} />) 
+                    }
                     <Typography sx={{ fontSize: '16px', fontWeight: 500, opacity: 0.7, mr: '6px' }}>좋아요 {board?.likeCount}</Typography>
-                    <IconButton sx={{ height: '24px', width: '24px' }}>
-                        <KeyboardArrowDownOutlinedIcon />
+                    <IconButton sx={{ height: '24px', width: '24px' }} onClick={() => setOpenLike(!openLike)}>
+                        { openLike ? 
+                            (<KeyboardArrowUpOutlinedIcon />) : 
+                            (<KeyboardArrowDownOutlinedIcon />) 
+                        }
                     </IconButton>
                 </Box>
                 <Box sx={{ display: 'flex' }}>
                     <CommentOutlinedIcon sx={{ height: '24px', width: '24px', mr: '6px', opacity: 0.7 }} />
                     <Typography sx={{ fontSize: '16px', fontWeight: 500, opacity: 0.7, mr: '6px' }}>댓글 {board?.commentCount}</Typography>
-                    <IconButton sx={{ height: '24px', width: '24px' }}>
-                        <KeyboardArrowDownOutlinedIcon />
+                    <IconButton sx={{ height: '24px', width: '24px' }} onClick={() => setOpenComment(!openComment)}>
+                        { openComment ? 
+                            (<KeyboardArrowUpOutlinedIcon />) : 
+                            (<KeyboardArrowDownOutlinedIcon />) 
+                        }
                     </IconButton>
                 </Box>
             </Box>
         </Box>
-        <Box></Box>
-        <Box></Box>
+        { openLike && (
+            <Box sx={{ mt: '20px' }}>
+                <Card variant='outlined' sx={{ p: '20px' }}>
+                    <Typography>좋아요 {board?.likeCount}</Typography>
+                    <Box></Box>
+                </Card>
+            </Box>
+        ) }
+        
+        <Box>
+
+        </Box>
     </Box>
   )
 }
