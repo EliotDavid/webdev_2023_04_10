2f60afe4da276b5ea863f36ceb700da43304a914
devJiraynor <jiraynor@naver.com>
2023-03-10 15:49:35 +0900

    2023.03.10. 15:50

--- board/src/main/java/com/koreait/board/provider/TokenProvider.java
+++ board/src/main/java/com/koreait/board/provider/TokenProvider.java
@@ -4,12 +4,13 @@ import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
-@Service
+@Component
 public class TokenProvider {
     
     public String create() {
@@ -32,6 +33,19 @@ public class TokenProvider {
 
         return jwt;
                     
+    }
+
+    public String validate (String jwt) {
+
+        //? 매개변수로 받은 jwt를 소유하고있는 secureKey를 사용해서 복호화 (디코딩)
+        Claims claims = Jwts
+            .parser()
+            .setSigningKey("SecureKey")
+            .parseClaimsJws(jwt)
+            .getBody();
+
+        return claims.getSubject();
+
     }
 
 }
