3df3e4abbadab7cbf063471c46a9f18cb4391233
devJiraynor <jiraynor@naver.com>
2023-03-14 15:50:15 +0900

    2023.03.14. 15:50

--- board/src/main/java/com/jihoon/board/controller/AuthController.java
+++ board/src/main/java/com/jihoon/board/controller/AuthController.java
@@ -1,10 +1,16 @@
 package com.jihoon.board.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jihoon.board.common.constant.ApiPattern;
+import com.jihoon.board.dto.request.auth.SignUpDto;
+import com.jihoon.board.dto.response.ResponseDto;
+import com.jihoon.board.dto.response.auth.SignUpResponseDto;
 import com.jihoon.board.service.AuthService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,11 +22,12 @@ public class AuthController {
 
     @Autowired private AuthService authService;
 
-    private String SIGN_UP = "/sign-up";
+    private final String SIGN_UP = "/sign-up";
     
     @PostMapping(SIGN_UP)
-    public ? signUp(?) {
-        
+    public ResponseDto<SignUpResponseDto> signUp(@Valid @RequestBody SignUpDto requestBody) {
+        ResponseDto<SignUpResponseDto> response = authService.signUp(requestBody);
+        return response;
     }
     
 }
--- board/src/main/java/com/jihoon/board/service/AuthService.java
+++ board/src/main/java/com/jihoon/board/service/AuthService.java
@@ -1,10 +1,49 @@
 package com.jihoon.board.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import com.jihoon.board.common.constant.ResponseMessage;
+import com.jihoon.board.dto.request.auth.SignUpDto;
+import com.jihoon.board.dto.response.ResponseDto;
+import com.jihoon.board.repository.UserRepository;
 
 @Service
 public class AuthService {
     
-    
+    @Autowired private UserRepository userRepository;
+
+    private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    public ResponseDto<SignUpDto> signUp(SignUpDto dto) {
+
+        SignUpDto data = null;
+
+        String email = dto.getEmail();
+        String telNumber = dto.getTelNumber();
+        String password = dto.getPassword();
+
+        try {
+
+            boolean hasEmail = userRepository.existsByEmail(email);
+            if (hasEmail) return ResponseDto.setFailed(ResponseMessage.EXIST_EMAIL);
+
+            boolean hasTelNumber = userRepository.existsByTelNumber(telNumber);
+            if (hasTelNumber) return ResponseDto.setFailed(ResponseMessage.EXIST_TEL_NUMBER);
+
+            String encodedPassword = passwordEncoder.encode(password);
+
+            
+
+        } catch(Exception exception) {
+            exception.printStackTrace();
+            return ResponseDto.setFailed(ResponseMessage.DATABASE_ERROR);
+        }
+
+        return ResponseDto.setSuccess(ResponseMessage.SUCCESS, data);
+
+    }
 
 }
