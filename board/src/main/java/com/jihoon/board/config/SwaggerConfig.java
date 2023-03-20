8b4f34d89c8d62bcadb3dbdbb9d6a870105fb880
devJiraynor <jiraynor@naver.com>
2023-03-20 17:03:05 +0900

    2023.03.20. 16:50

---
+++ board/src/main/java/com/jihoon/board/config/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package com.jihoon.board.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+            .title("Hoon`s board REST API")
+            .description("Spring boot로 작성한 REST API 명세서")
+            .version("1.0.0")
+            .build();
+    }
+
+}
