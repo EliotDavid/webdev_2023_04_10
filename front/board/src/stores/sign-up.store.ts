f0e6fbf30aeadcbde57b6581de4e7f96b9ea37ba
optimistprimer <optimistprimer@naver.com>
2023-04-10 15:29:45 +0900

    2023_04_10_first

--- front/board/src/stores/sign-up.store.ts
+++ front/board/src/stores/sign-up.store.ts
@@ -29,8 +29,9 @@ interface ISignUpStore {
     signUpError: boolean;
     setSignUpError: (signUpError: boolean) => void;
 
-    emailPatternCheck: boolean | null;
-    setEmailPatternCheck: (emailPatternCheck: boolean) => void;
+    emailPatternCheck: boolean | null; // 타입스크립트에서는 null만 선언을 해줄 수 없기 떄문에 null 또는 boolean으로 2개로 선언해준거라고 함
+    setEmailPatternCheck: (emailPatternCheck: boolean) => void; // 추상메서드를 선언하는 코드인데 각 클래스에는 setter가 있어야하므로 이 추상메서드에 타입이 boolean인 속성이름은 emailPatternCheck이라는걸 받는다는 의미이고 이 추상메서드는 void형태로 return을 해준다는 의미로 쓴 코드라고 함
+                                                                // 근데 속성선언은 null값 또는 boolean형태로 받아놓고 인자값의 타입으로 boolean형태로만 받는 이유는 애초에 필수값으로 지정해뒀기 때문에 null값은 존재할 수 없다는 이유이고 속성이 set 될 때는 반드시 그 값이 있어야하므로 이 추상메서드를 선언할 때는 null타입이 아니라 boolean타입만 존재하는 것 같음 
     emailValidate: boolean | null;
     setEmailValidate: (emailValidate: boolean) => void;
 
@@ -49,7 +50,8 @@ interface ISignUpStore {
 }
 
 //^ create 메서드를 사용해서 store를 생성
-const useStore = create<ISignUpStore>((set) => ({
+const useStore = create<ISignUpStore>((set) => ({ // 이제 이 ISignUpStore라는 인터페이스로 클래스를 만들기위한 코드인데 여기서<IsginUPStore> 제네릭 앞에 있는 create 메서드는 react에서 제공하는? 타입스크립트에서 제공하는? zustand 객체가 가지고 있는 메서드이고
+                                                  // 이 ISginUpStore인터페이스로 useStore라는 클래스(객체)를 만들기 위한 코드라고 함   
     email: '',
     password: '',
     passwordCheck: '',
@@ -59,7 +61,7 @@ const useStore = create<ISignUpStore>((set) => ({
     addressDetail: '',
     emailMessage: '',
     setEmail: (email) => {
-        const emailValidator = /^[A-Za-z0-9]*@[A-Za-z0-9]([-.]?[A-Za-z0-9])*\.[A-Za-z0-9]{2,3}$/;
+        const emailValidator = /^[A-Za-z0-9]*@[A-Za-z0-9]([-.]?[A-Za-z0-9])*\.[A-Za-z0-9]{2,3}$/; // 이거 뭔 뜻이지?
         const isMatched = emailValidator.test(email);
         const emailMessage = isMatched ? '' : '이메일 주소 포맷이 맞지 않습니다.';
         set((state) => ({...state, email, emailMessage}))
@@ -70,6 +72,29 @@ const useStore = create<ISignUpStore>((set) => ({
     setTelNumber: (telNumber) => set((state) => ({...state, telNumber})),
     setAddress: (address) => set((state) => ({...state, address})),
     setAddressDetail: (addressDetail) => set((state) => ({...state, addressDetail})),
+
+    signUpError: false,
+    setSignUpError: (signUpError: boolean) => set((state) => ({...state, signUpError})),
+
+    emailPatternCheck: null, // 이메일패턴체크는 기본값으로 null을 집어넣고
+    setEmailPatternCheck: (emailPatternCheck: boolean) => set((state) => ({...state,  emailPatternCheck})),
+    emailValidate: null,
+    setEmailValidate: (emailValidate: boolean) => set((state) => ({...state, emailValidate})),
+
+    passwordPatternCheck: null,
+    setPasswordPatternCheck: (passwordpPatternCheck: boolean) => set((state) => ({...state, passwordPatternCheck:})),
+    passwordValidate :null,
+    setPasswrodValidate: (passwordValidate: boolean) => set((state) => ({...state, passwordValidate})),
+
+    nicknameValidate: null,
+    setNicknameValidate: (passwordValidate: boolean) => set((state) => ({...state, nicknameValidate})),
+
+    telNumberPatternCheck: null,
+    setTelNumberPatternCheck: (passwordValidate: boolean) => set((state) => ({...state, telNumberPatternCheck})),
+    telNumberValidate: null,
+    setTelNumberValidate:  (passwordValidate: boolean) => set((state) => ({...state, telNumberValidate}))
+
+
 }))
 
 export default useStore;
